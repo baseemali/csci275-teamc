468944c1296786abc4b2297513d3aadac7ee953e
Baseem Ali <b4seemali@gmail.com>
2026-08-09 03:47:08 -0700

    chore: use dynamic PORT for deployment

--- vendor-profile-management/backend/server.js
+++ vendor-profile-management/backend/server.js
@@ -25,6 +25,4 @@ app.get('/', (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(` Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
